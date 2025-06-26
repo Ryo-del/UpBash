4ebe273c17bb03e67ee2eb699d656bfe7a5f88e7
Ryo <Ryo@asdd>
2025-06-26 23:21:17 +0300

    update lv1

--- lv1/main.sh
+++ lv1/main.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-read -p "Enter the directory name: " dir 
+dir="$1"
+if [ -z "$dir" ]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+if [ ! -d "$dir" ]; then
+    echo "Directory does not exist"
+    exit 1
+fi
 
 echo "Всего файлов: $(find "$dir" -maxdepth 1 -type f | wc -l)"
 echo "Всего директорий: $(find "$dir" -maxdepth 1 -type d | wc -l)"
 echo "Список файлов (сортировка по размеру) сохранён в report.txt"
 touch "report.txt" 
-echo $(ls -S "$dir") > report.txt
+ls -1hS "$dir" > report.txt
+
